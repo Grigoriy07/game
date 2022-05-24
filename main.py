fa33d659c966c2743dd020fa69689250acf32078
Grigoriy07 <grigorian.200253@gmail.com>
2022-05-24 12:31:56 +0300

    [Added] player

--- main.py
+++ main.py
@@ -47,3 +47,59 @@ mobs_images_and_shield = [meteor_20_image, meteor_40_image, meteor_70_image]
 
 shield_is_active = False
 recordings = []
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = player_image
+        self.image_bullet = pygame.Surface((2, 10))
+        self.rect = self.image.get_rect()
+        self.rect.y = 560
+        self.rect.x = 215
+        self.speedx = 8
+        self.last_hit = 0
+        self.recharge = 200
+        self.bullet_y = self.rect.y
+
+    def update(self, left, right, shoot):
+        if left:
+            self.rect.x -= self.speedx
+        elif right:
+            self.rect.x += self.speedx
+        if shoot:
+            now_hit = pygame.time.get_ticks()
+            if now_hit - self.last_hit > self.recharge:
+                self.last_hit = now_hit
+                self.shoot()
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.bullet_y, self.image_bullet)
+        if recording_go:
+            all_sprites_c.add(bullet)
+            bullets_c.add(bullet)
+        else:
+            all_sprites.add(bullet)
+            bullets.add(bullet)
+
+    def shoot_rec(self):
+        bullet = Bullet(self.rect.centerx, self.bullet_y, self.image_bullet)
+        all_sprites_c.add(bullet)
+        bullets_c.add(bullet)
+
+
+
+def load_image(name):
+    fullname = os.path.join(name)
+    if not os.path.isfile(fullname):
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+def draw_text(x, y, text, surface, size, color):
+    f_1 = pygame.font.Font(None, size)
+    text_s = f_1.render(str(text), False, color)
+    surface.blit(text_s, (x, y))
